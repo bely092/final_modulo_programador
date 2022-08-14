d92d1a2fe80e73ce6e7a18cd23801c9fbba15a3c
bely092 <koen.lion92@gmail.com>
2022-08-13 22:50:57 -0300

    :seedling: add archivo python

---
+++ index.py
@@ -0,0 +1,9 @@
+
+#10: Crear una lista denominada "Clientes"
+#con los nombres de los diferentes dueños de perros.
+#Ordenarla alfabeticamente y mostrarla por pantalla
+
+
+clientes=['Juan','Mario','Ariel','Josefina','Marianella']
+lista_cliente=sorted(clientes)
+print(lista_cliente)
